3af0b98d6ef157c017c87bf57a8f70e10452ac34
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2025-04-24 17:07:53 +0530

    commit by Prabir 24-04-2025

--- app/src/main/java/com/genius/imfa/Leave/fragment/ApplicationFragment.java
+++ app/src/main/java/com/genius/imfa/Leave/fragment/ApplicationFragment.java
@@ -66,6 +66,7 @@ import com.developers.imagezipper.ImageZipper;
 //import com.genius.imfa.Model.EncashmentItemModel;
 import com.genius.imfa.Utility.FileUtils;
 //import com.genius.imfa.adapter.LeaveEncashmentAdapter;
+import com.genius.imfa.Utility.RefreshAccessToken;
 import com.genius.imfa.common.AndroidXCameraActivity;
 import com.genius.imfa.Leave.LeaveApplicationActivity;
 import com.genius.imfa.Model.CompOffDetailsModel;
@@ -1147,18 +1148,31 @@ public class ApplicationFragment extends Fragment {
                     @Override
                     public void onError(ANError error) {
                         if (error.getErrorCode()==401){
-                            JSONObject obj=new JSONObject();
-                            try {
-                                obj.put("MasterID",encrypt(pref.getMasterId(),SECRET_KEY));
-                                obj.put("Password",encrypt(pref.getPassword(),SECRET_KEY));
-                                obj.put("IMEI","0");
-                                obj.put("DeviceID","0");
-                                obj.put("DeviceType","A");
-                                obj.put("SecurityCode",pref.getSecurityCode());
-                                login(obj);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
+                            RefreshAccessToken.apiCall(getContext(), new RefreshAccessToken.OnResponse() {
+                                @Override
+                                public void onPostCall() {
+
+                                }
+
+                                @Override
+                                public void onSuccess() {
+                                    JSONObject jsonObject=new JSONObject();
+                                    try {
+                                        jsonObject.put("CompanyID",pref.getEmpClintId());
+                                        jsonObject.put("EmployeeID",pref.getEmpId());
+                                        jsonObject.put("ApproverID",pref.getEmpId());
+                                        jsonObject.put("SecurityCode",pref.getSecurityCode());
+                                        getLeaveAllDetails(jsonObject);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(String message) {
+
+                                }
+                            });
                         } else {
                             llLoader.setVisibility(View.GONE);
                         }
@@ -2706,53 +2720,4 @@ public class ApplicationFragment extends Fragment {
             etTotalNumberOfEncasement.setText(String.valueOf(totalEncasementValue));
         }
     }*/
-
-    private void login(JSONObject jsonObject) {
-        final ProgressDialog pd = new ProgressDialog(getContext());
-        pd.setMessage("Loading..");
-        pd.setCancelable(false);
-        pd.show();
-        AndroidNetworking.post(Api.sLogin)
-                .addJSONObjectBody(jsonObject)
-                .setTag("uploadTest")
-                .setPriority(Priority.HIGH)
-                .build()
-
-                .getAsJSONObject(new JSONObjectRequestListener() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        JSONObject job1 = response;
-                        Log.e("response12", "@@@@@@" + job1);
-                        pd.dismiss();
-
-                        int Response_Code = job1.optInt("Response_Code");
-                        if (Response_Code == 101) {
-                            // Toast.makeText(getApplicationContext(),responseText,Toast.LENGTH_LONG).show();
-                            JSONArray responseData = job1.optJSONArray("Response_Data");
-                            for (int i = 0; i < responseData.length(); i++) {
-                                JSONObject obj = responseData.optJSONObject(i);
-                                String Genius_Access_Token=obj.optString("Genius_Access_Token");
-                                pref.saveAccessToken(Genius_Access_Token);
-                                // boolean _status = job1.getBoolean("status");
-
-                                JSONObject jsonObject=new JSONObject();
-                                try {
-                                    jsonObject.put("CompanyID",pref.getEmpClintId());
-                                    jsonObject.put("EmployeeID",pref.getEmpId());
-                                    jsonObject.put("ApproverID",pref.getEmpId());
-                                    jsonObject.put("SecurityCode",pref.getSecurityCode());
-                                    getLeaveAllDetails(jsonObject);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onError(ANError error) {
-                        pd.dismiss();
-                    }
-                });
-    }
 }
